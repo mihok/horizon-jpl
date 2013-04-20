f6fc1bb5e9c10825ce971845823aab99417f866f
Siddarth Kalra <sid@sidkalra.com>
2013-04-20 19:09:59 -0400

    Cleanup of code; method documentation; improved url param parsing.

--- python-jpl-horizon/request.py
+++ python-jpl-horizon/request.py
@@ -16,9 +16,12 @@ import urllib
 
 class JplRequestHandler(BaseHTTPRequestHandler):
 
+    JSON_QUERY_PARAM_START_INDEX = 11
+
     def do_GET(self):
-        if "/api?query=" in self.path:
+        if self.path[:self.JSON_QUERY_PARAM_START_INDEX] == "/api?query=":
             json_obj = self.__get_query_json(self.path)
+            
             if json_obj is None:
                 self.__send_http_response_400("Invalid json provided.")
                 return
@@ -30,10 +33,6 @@ class JplRequestHandler(BaseHTTPRequestHandler):
             if not json_validation["success"]:
                 self.__send_http_response_400(json_validation["message"])
             else:
-                self.send_response(200)
-                self.send_header("Content-type", "application/json")
-                self.end_headers()
-            
                 json_obj = json_obj["horizons-api"]
                 response = {
                     "version": None
@@ -43,28 +42,36 @@ class JplRequestHandler(BaseHTTPRequestHandler):
                 horizon_data = Horizon()
                 response["version"] = horizon_data.version()
                 
-                if json_obj["query_type"] == "list" and "filters" in json_obj:
+                #decision tree for what horizon method to run
+                if json_obj["query_type"] == "list":
                     if json_obj["filters"]["body_type"] == "mb":
                         response["mb"] = horizon_data.major() 
                     elif json_obj["filters"]["body_type"] == "sb":
                         response["sb"] = horizon_data.minor()
-                elif json_obj["query_type"] == "id" and "filters" in json_obj and "value" in json_obj["filters"]:
-                    id = json_obj["filters"]["value"]
-                    
-                    if id.isdigit():
-                        response["id"] = horizon_data.get(id)
+                elif json_obj["query_type"] == "id":
+                    response["id"] = horizon_data.get(json_obj["filters"]["value"])
+                elif json_obj["query_type"] == "name": 
+                    response["name"] = horizon_data.get(json_obj["filters"]["value"])
             
+                #set success headers
+                self.send_response(200)
+                self.send_header("Content-type", "application/json")
+                self.end_headers()
+                
                 #do the JSON magic
                 json.dump(response, self.wfile)
 
+
+    "send back a bad request header with a message"
     def __send_http_response_400(self, message):
         self.send_response(400)
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(message)        
 
+    "read the url path and load the JSON"
     def __get_query_json(self, path):
-        json_str = urllib.unquote_plus(path[11:])
+        json_str = urllib.unquote_plus(path[self.JSON_QUERY_PARAM_START_INDEX:])
         print json_str
 
         try:
@@ -72,11 +79,12 @@ class JplRequestHandler(BaseHTTPRequestHandler):
         except:
             return None
 
+    "validate the json request object"
     def __is_json_obj_valid(self, json_obj):
         required_keys = ("version", "response_type", "query_type", "filters")
-        query_types = ("id", "name")
-        body_types = ("mb", "sb")
-        response_types = ("json")
+        query_types = ("id", "name", "list") #to be used
+        list_body_types = ("mb", "sb") #to be used
+        response_types = ("json") #to be used
 
         result = {"success": False, "message": "Failed to validate request."}
 
@@ -88,6 +96,11 @@ class JplRequestHandler(BaseHTTPRequestHandler):
             if not r_key in json_obj["horizons-api"]:
                 result["message"] = "Failed. Second level key '%s' missing." % r_key
                 return result
+
+         #TODO -- more in-depth json query validation
+         #if json_obj["horizons-api"]["query_type"] == "list":
+         #elif json_obj["horizons-api"]["query_type"] == "id":
+         #elif json_obj["horizons-api"]["query_type"] == "name":
 
         result["success"] = True
         result["message"] = "Success"
