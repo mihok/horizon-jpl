659e3e4af356346c3e20f88f8ada099a528b0e6d
Matthew Mihok <m.mihok@zerotwomedia.com>
2013-05-01 00:43:02 -0400

    updating pypi package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='HorizonJPL',
-    version='0.1.3',
+    version='0.1.4',
     author='Matthew Mihok',
     author_email='matthew.mihok@niewma.com',
     packages=['horizon', 'horizon.test'],
